c00ab3603732c18e953d7116ac92601ced0df2bf
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-12 18:53:02 +0800

    >  compile NEMU 3019244255 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:53:02 up 1 day,  4:16,  1 user,  load average: 0.10, 0.13, 0.09 36517cedc7049187e994e470738c2c5475e9e6a6

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,6 +1,6 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
-
+#include "nemu.h"
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
@@ -78,11 +78,51 @@ void free_wp(WP *wp)
 	wp->EXp[0] = '\0';
 }
 
+bool check_up()
+{
+	WP *f;
+	f = head;
+	bool flag = true;
+	bool suc;
+	while(f != NULL)
+	{
+		uint32_t tmp = expr(f->EXp, &suc);
+		if(!suc)
+		assert(0);
+		if(tmp != f->Val)
+		{
+			flag = false;
+			if(f->klx)
+			{
+				printf("Hit breakpoint %d at 0x%08x\n", f->NO, cpu.eip);
+				f = f->next;
+				continue;
+			}
+			printf("Watchpoint %d: %s\n", f->NO, f->EXp);
+			f->Val = tmp;	
+		}
+		f = f->next;
+	}
+	return flag;
+}
 
+void delete_wp(int No)
+{
+	WP *f;
+	f = &wp_pool[No];
+	free_wp(f);
+}
 
-
-
-
+void info_wp()
+{
+	WP *f;
+	f = head;
+	while(f != NULL)
+	{
+		printf("Watchpoint %d: %s = %d\n", f->NO, f->EXp, f->Val);
+		f = f->next;
+	}
+}
 
 
 
