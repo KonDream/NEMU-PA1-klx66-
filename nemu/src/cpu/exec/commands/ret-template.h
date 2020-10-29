704ddee9be14ca181c2967d4da7d40218203a66c
tracer-ics2016 <tracer@njuics.org>
2020-10-30 01:49:30 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:49:30 up 8 days,  4:14,  1 user,  load average: 1.60, 0.81, 0.74 110053f27f40a04c242cbf148841fd32204c802e

--- nemu/src/cpu/exec/commands/ret-template.h
+++ nemu/src/cpu/exec/commands/ret-template.h
@@ -14,9 +14,14 @@ make_helper(concat(ret_i_, SUFFIX))
 {
     int addr = instr_fetch(eip + 1, 2);
     cpu.eip = MEM_R(REG(R_ESP));
+    if(DATA_BYTE == 2)
     cpu.eip &= 0xffff;
-    REG(R_ESP) += 2 + addr;
-    print_asm_template1();
+    REG(R_ESP) += DATA_BYTE;
+    int i;
+    for(i = 0; i < addr; i += DATA_BYTE)
+    MEM_W(REG(R_ESP) + i, 0);
+    REG(R_ESP) += addr;
+    print_asm("ret $0x%x", addr);
     return 1;
 }
 
