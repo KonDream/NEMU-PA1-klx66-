f33ae93e1e76264a2d2b8f05bba30a1b9e621a36
tracer-ics2016 <tracer@njuics.org>
2020-11-03 23:37:10 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:37:10 up 4 days,  2:50,  1 user,  load average: 1.09, 0.82, 0.66 be9e7b93e0c009979582b2565c48ac15d1cb90db

--- nemu/src/cpu/exec/control-tran/call-template.h
+++ nemu/src/cpu/exec/control-tran/call-template.h
@@ -16,10 +16,9 @@ make_helper(concat(call_rm_, SUFFIX))
 {
     int length = concat(decode_rm_, SUFFIX) (cpu.eip + 1);
     reg_l(R_ESP) -= DATA_BYTE;
-    swaddr_write(reg_l(R_ESP), 4, cpu.eip + length);
-    DATA_TYPE_S dis = op_src->val;
-    print_asm("call %x", dis);
-    cpu.eip = dis - length - 1;
+    MEM_W(reg_l(R_ESP), cpu.eip + length);
+    cpu.eip = (DATA_TYPE_S)op_src->val - length - 1;
+    print_asm("call 0x%x\n", cpu.eip + length + 1);
     return length + 1;
 }
 
