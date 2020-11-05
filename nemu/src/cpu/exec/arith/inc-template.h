4f0466b7c276e2e4dacbf47964669c664710eaf4
tracer-ics2016 <tracer@njuics.org>
2020-11-06 02:35:17 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:35:17 up 6 days,  5:49,  1 user,  load average: 0.92, 1.10, 1.05 211b32693f43d231fa35cd8f3e0fa8fc19bbbc28

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -7,8 +7,16 @@ static void do_execute () {
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	//panic("please implement me");
+	cpu.EFLAGS.CF = 0;
+	cpu.EFLAGS.OF = 0;
+	cpu.EFLAGS.ZF = !result;
+	cpu.EFLAGS.OF = result < op_src->val;
+	cpu.EFLAGS.SF = result >> ((DATA_BYTE << 3) - 1);
+	result ^= result >> 4;
+	result ^= result >> 2;
+	result ^= result >> 1;
+	cpu.EFLAGS.PF = !(result & 1);
 	print_asm_template1();
 }
 
