10f0f8ec50e8c45b1557adb3ed2b7b99a99d8b55
tracer-ics2016 <tracer@njuics.org>
2020-11-05 18:51:34 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:51:34 up 5 days, 22:05,  1 user,  load average: 1.02, 0.89, 0.66 1b9b5a28ff2e0e4d2a78d8910b7b88ac2015f05

--- nemu/src/cpu/exec/logic/shl-template.h
+++ nemu/src/cpu/exec/logic/shl-template.h
@@ -11,8 +11,8 @@ static void do_execute () {
 	OPERAND_W(op_dest, dest);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	//panic("please implement me");
+	cpu.EFLAGS.CF = op_dest->val;
 	print_asm_template2();
 }
 
