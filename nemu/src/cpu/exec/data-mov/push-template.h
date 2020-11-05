1bc76a9940509090680dd91082c1e9cdd24dbe73
tracer-ics2016 <tracer@njuics.org>
2020-11-05 15:59:31 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:59:31 up 5 days, 19:13,  1 user,  load average: 0.86, 0.96, 0.68 a22bb36ecb05072224ec83bc7ed9c982c82bfa9a

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -10,7 +10,7 @@ static void do_execute()
         MEM_W(cpu.esp, op_src->val);
     }
     else {
-        if(DATA_BYTE == 1)
+       // if(DATA_BYTE == 1)
         op_src->val = (int8_t)op_src->val;
         reg_l(R_ESP) -= 4;
         swaddr_write(reg_l(R_ESP), 4, op_src->val);
