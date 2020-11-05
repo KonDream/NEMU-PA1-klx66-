9680a28f83f9598e7844cce3d668905635730150
tracer-ics2016 <tracer@njuics.org>
2020-11-06 00:54:08 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:54:08 up 6 days,  4:07,  1 user,  load average: 1.12, 0.85, 0.79 d473542649f22628ab8170a87bcb3b3f3ae5a4b4

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -10,7 +10,18 @@ static void do_execute()
     else
         ans = op_dest->val - (op_src->val - cpu.EFLAGS.CF);
     OPERAND_W(op_dest, ans);
-
+    int len = (DATA_BYTE << 3) - 1;
+    int k1 = op_dest->val >> len;
+    int k2 = op_src->val >> len;
+    cpu.EFLAGS.ZF = !ans;
+    cpu.EFLAGS.SF = ans >> len;
+    cpu.EFLAGS.CF = op_dest->val < (op_src->val + cpu.EFLAGS.CF);
+    cpu.EFLAGS.OF = (k1 != k2 && k1 == cpu.EFLAGS.SF);
+    ans ^= ans >> 4;
+    ans ^= ans >> 2;
+    ans ^= ans >> 1;
+    cpu.EFLAGS.PF = !(ans & 1);
+    print_asm_template2();
 }
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
