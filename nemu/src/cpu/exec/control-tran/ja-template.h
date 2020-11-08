54721e825f31016b97084c2f27e4f14b333debc8
tracer-ics2016 <tracer@njuics.org>
2020-11-08 14:05:15 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:05:15 up  2:28,  1 user,  load average: 1.14, 1.14, 0.77 95b92be0ca5c716c83ddac67f4152224de030f82

--- nemu/src/cpu/exec/control-tran/ja-template.h
+++ nemu/src/cpu/exec/control-tran/ja-template.h
@@ -5,7 +5,7 @@
 static void do_execute()
 {
     DATA_TYPE_S tmp = op_src->val;
-    print_asm("jbe %x\n", cpu.eip + tmp + DATA_BYTE + 1);
+    print_asm("ja %x\n", cpu.eip + tmp + DATA_BYTE + 1);
     if(cpu.EFLAGS.CF == 0 && cpu.EFLAGS.ZF == 0)
     cpu.eip += tmp;
 }
--- nemu/src/cpu/exec/control-tran/jge-template.h
+++ nemu/src/cpu/exec/control-tran/jge-template.h
@@ -5,7 +5,7 @@
 static void do_execute()
 {
     DATA_TYPE_S tmp = op_src->val;
-    print_asm("jbe %x\n", cpu.eip + tmp + DATA_BYTE + 1);
+    print_asm("jge %x\n", cpu.eip + tmp + DATA_BYTE + 1);
     if(cpu.EFLAGS.SF == cpu.EFLAGS.OF)
     cpu.eip += tmp;
 }
