5da24ba61c83e8249316991891db0615e0cde0f8
tracer-ics2016 <tracer@njuics.org>
2020-11-09 00:13:47 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:13:47 up 12:37,  1 user,  load average: 0.82, 0.77, 0.71 f056690ca38404b3700ecec4971e744108799a6

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -5,7 +5,17 @@
 static void do_execute () {
 	DATA_TYPE result = op_dest->val + op_src->val + cpu.EFLAGS.CF;
 	OPERAND_W(op_dest, result);
-
+	int length = (DATA_BYTE << 3) - 1;
+    int k1 = (op_dest->val) >> length;
+    int k2 = (op_src->val) >> length;
+    cpu.EFLAGS.ZF = !result;
+    cpu.EFLAGS.SF = result >> length;
+    cpu.EFLAGS.CF = result < op_dest->val;
+    cpu.EFLAGS.OF = (k1 == k2 && k1 != cpu.EFLAGS.SF);
+    result ^= result >> 4;
+    result ^= result >> 2;
+    result ^= result >> 1;
+    cpu.EFLAGS.PF = !(result & 1);
 	print_asm_template2();
 }
 
