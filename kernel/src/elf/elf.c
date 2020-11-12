44a35c04df520082709c8cc0f2d22c8c38ff5edf
tracer-ics2016 <tracer@njuics.org>
2020-11-12 17:48:16 +0800

    >  compile kernel 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:48:16 up 4 days,  6:11,  1 user,  load average: 0.59, 0.57, 0.48 c42b2ce19c3da17e59f6701847962dac61278861

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,22 +37,22 @@ uint32_t loader() {
 
 	/* Load each program segment */
 	//panic("please implement me");
-	int i = 0, phlen = elf->e_phnum;
-	ph = (Elf32_Phdr *)(buf + elf->e_phoff);
-	for(; i < phlen; ++ i) {
+	int i = 0;
+	ph = (void *)(buf + elf->e_phoff);
+	for(; i < elf->e_phnum; ++ i, ++ ph) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			ramdisk_read((uint8_t *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
+			ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 			memset((void*)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
-			++ ph;
+			
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
