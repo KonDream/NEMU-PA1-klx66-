2420ad44265b863efc74727b7f27df802cfb2c2e
tracer-ics2016 <tracer@njuics.org>
2021-01-06 21:06:42 +0800

    >  compile NEMU 3019244255 klx Linux klx-virtual-machine 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:06:42 up  3:27,  1 user,  load average: 0.45, 2.38, 5.80 6397d999ce2cb11080427bb7dbb72e2e522f3e16

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include "burst.h"
 
+void ddr3_read_replace(hwaddr_t addr, void *data);
+void dram_write(hwaddr_t addr, size_t len, uint32_t data);
+void ddr3_write_replace(hwaddr_t addr, void *data, uint8_t *mask);
+
 void init_cache(){
     int i;
     for(i = 0; i < Cache_L1_Size / Cache_Block_L1_Size;i ++)
@@ -27,4 +31,30 @@ int read_cache1(hwaddr_t addr){
     cache1[i].valid = 1;
     cache1[i].tag = tag;
     return i;
+}
+
+void write_cache1(hwaddr_t addr, size_t len, uint32_t data){
+    uint32_t group_idx = (addr >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
+    uint32_t tag = (addr >> (Cache_L1_Group_Bit + Cache_L1_Block_Bit));
+    uint32_t offset = addr & (Cache_Block_L1_Size - 1);
+
+    int i,group = group_idx * Cache_L1_Way_Size;
+    for (i = group + 0;i < group + Cache_L1_Way_Size;i ++){
+        if (cache1[i].valid == 1 && cache1[i].tag == tag){// WRITE HIT
+            /*write through*/
+            if (offset + len > Cache_Block_L1_Size)
+            {
+                dram_write(addr,Cache_Block_L1_Size - offset,data);
+                memcpy(cache1[i].data + offset, &data, Cache_Block_L1_Size - offset);
+                write_cache1(addr + Cache_Block_L1_Size - offset, len -  (Cache_Block_L1_Size - offset), data >>  (Cache_Block_L1_Size - offset));
+            }
+            else
+            {
+                dram_write(addr,len,data);
+                memcpy(cache1[i].data + offset, &data, len);
+                
+            }
+            return;
+        }
+    }
 }
