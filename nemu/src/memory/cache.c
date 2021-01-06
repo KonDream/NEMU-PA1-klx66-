2958976903eff9695ee9763e5afcd6b5598283c6
tracer-ics2016 <tracer@njuics.org>
2021-01-06 21:31:30 +0800

    >  compile NEMU 3019244255 klx Linux klx-virtual-machine 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:31:30 up  3:52,  1 user,  load average: 1.11, 0.98, 1.81 e7ddb42a1b948f344874feb26e24fc6844460825

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "burst.h"
+#include <time.h>
 
 void ddr3_read_replace(hwaddr_t addr, void *data);
 void dram_write(hwaddr_t addr, size_t len, uint32_t data);
@@ -19,6 +20,7 @@ void init_cache(){
 int read_cache1(hwaddr_t addr){
     uint32_t group_idx = (addr >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
     uint32_t tag = (addr >> (Cache_L1_Group_Bit + Cache_L1_Block_Bit));
+    uint32_t block_start = (addr >> Cache_L1_Block_Bit) << Cache_L1_Block_Bit;
     int i, group = group_idx * Cache_L1_Way_Size;
     for(i = group + 0; i < group + Cache_L1_Way_Size; i ++){
         if (cache1[i].valid == 1 && cache1[i].tag == tag){
@@ -28,6 +30,17 @@ int read_cache1(hwaddr_t addr){
             return i;
         }
     }
+
+    #ifdef Test
+        test_time += 200;
+    #endif
+    srand(time(0));
+    i = group + rand() % Cache_L1_Way_Size;
+        /*new content*/
+    int j;
+    for (j = 0;j < Cache_Block_L1_Size / BURST_LEN;j ++)
+        ddr3_read_replace(block_start + BURST_LEN * j, cache1[i].data + BURST_LEN * j);
+    
     cache1[i].valid = 1;
     cache1[i].tag = tag;
     return i;
