f5e0a8d0802d63ae6ca55ba79e54c7e43d06a1ab
tracer-ics2016 <tracer@njuics.org>
2021-01-07 00:59:13 +0800

    >  compile NEMU 3019244255 klx Linux klx-virtual-machine 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:59:13 up 13 min,  1 user,  load average: 0.64, 0.58, 0.42 70c62e84d93ed2207afc9298205b73859b2bd374

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -25,6 +25,29 @@ typedef struct{
 
 Cache_L1 cache1[Cache_L1_Size/Cache_Block_L1_Size];
 
+#define Cache_Block_L2_Size 64  //  cache block 存储空间的大小为 64B
+#define Cache_L2_Size 4 * 1024 * 1024   //  cache 存储空间的大小为 4MB
+#define Cache_L2_Way_Bit 4  //  16-way set associative  2^4
+#define Cache_L2_Group_Bit 12
+#define Cache_L2_Block_Bit 6    //  64 = 2^6
+#define Cache_L2_Group_Size (1 << Cache_L2_Group_Bit)
+#define Cache_L2_Way_Size (1 << Cache_L2_Way_Bit)
+
+/*
+替换算法采用随机方式
+write back
+write allocate
+*/
+
+typedef struct{
+    uint8_t data[Cache_Block_L2_Size]; // 64B
+    uint32_t tag;
+    bool valid;
+    bool dirty;
+}Cache_L2;
+
+Cache_L2 cache2[Cache_L2_Size/Cache_Block_L2_Size];
+
 #ifdef Test
 uint64_t test_time;
 #endif
@@ -32,4 +55,8 @@ uint64_t test_time;
 void init_cache();
 int read_cache1(hwaddr_t);
 void write_cache1(hwaddr_t, size_t, uint32_t);
+
+int read_cache2(hwaddr_t);
+void write_cache2(hwaddr_t, size_t, uint32_t);
+
 #endif
--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -20,9 +20,9 @@ void init_cache(){
 int read_cache1(hwaddr_t addr){
     uint32_t group_idx = (addr >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
     uint32_t tag = (addr >> (Cache_L1_Group_Bit + Cache_L1_Block_Bit));
-    uint32_t block_start = (addr >> Cache_L1_Block_Bit) << Cache_L1_Block_Bit;
+    //uint32_t block_start = (addr >> Cache_L1_Block_Bit) << Cache_L1_Block_Bit;
     int i, group = group_idx * Cache_L1_Way_Size;
-    for(i = group + 0; i < group + Cache_L1_Way_Size; i ++){
+    for(i = group; i < group + Cache_L1_Way_Size; i ++){
         if (cache1[i].valid == 1 && cache1[i].tag == tag){
             #ifdef Test
                 test_time += 2;//HIT in Cache1
@@ -31,19 +31,65 @@ int read_cache1(hwaddr_t addr){
         }
     }
 
-    #ifdef Test
-        test_time += 200;
-    #endif
+     // Find in Cache2
+    int pl = read_cache2(addr);   
     srand(time(0));
     i = group + rand() % Cache_L1_Way_Size;
-        /*new content*/
-    int j;
-    for (j = 0;j < Cache_Block_L1_Size / BURST_LEN;j ++)
-        ddr3_read_replace(block_start + BURST_LEN * j, cache1[i].data + BURST_LEN * j);
+    memcpy(cache1[i].data, cache2[pl].data, Cache_Block_L1_Size);
+
+    // #ifdef Test
+    //     test_time += 200;
+    // #endif
+    // srand(time(0));
+    // i = group + rand() % Cache_L1_Way_Size;
+    //     /*new content*/
+    // int j;
+    // for (j = 0;j < Cache_Block_L1_Size / BURST_LEN;j ++)
+    //     ddr3_read_replace(block_start + BURST_LEN * j, cache1[i].data + BURST_LEN * j);
     
     cache1[i].valid = 1;
     cache1[i].tag = tag;
     //printf("%d\n", test_time);
+    return i;
+}
+
+int read_cache2(hwaddr_t addr){
+    uint32_t group_idx = (addr >> Cache_L2_Block_Bit) & (Cache_L2_Group_Size - 1);
+    uint32_t tag = (addr >> (Cache_L2_Group_Bit + Cache_L2_Block_Bit));
+    uint32_t block_start = (addr >> Cache_L2_Block_Bit) << Cache_L2_Block_Bit;
+
+    int i,group = group_idx * Cache_L2_Way_Size;
+    for (i = group; i < group + Cache_L2_Way_Size; i ++){
+        if (cache2[i].valid == 1 && cache2[i].tag == tag){  // READ HIT
+            #ifdef Test
+            test_time += 10;    //HIT in Cache2
+            #endif
+            return i;
+        }
+    }
+            #ifdef Test
+            test_time += 200;//NOT HIT in Cache2
+            #endif
+    srand(time(0));
+    i = group + rand() % Cache_L2_Way_Size;
+    /*write back*/
+    if (cache2[i].valid == 1 && cache2[i].dirty == 1){
+        uint8_t ret[BURST_LEN << 1];
+        uint32_t block_st = (cache2[i].tag << (Cache_L2_Group_Bit + Cache_L2_Block_Bit)) | (group_idx << Cache_L2_Block_Bit);
+        int w;
+        memset(ret,1,sizeof ret);
+        for (w = 0;w < Cache_Block_L2_Size / BURST_LEN; w++)
+            ddr3_write_replace(block_st + BURST_LEN * w, cache2[i].data + BURST_LEN * w,ret);
+        
+    }
+    /*new content*/
+    int j;
+    for (j = 0;j < Cache_Block_L2_Size / BURST_LEN;j ++)
+        ddr3_read_replace(block_start + BURST_LEN * j, cache2[i].data + BURST_LEN * j);
+
+    cache2[i].dirty = 0;
+    cache2[i].valid = 1;
+    cache2[i].tag = tag;
     return i;
 }
 
@@ -53,24 +99,52 @@ void write_cache1(hwaddr_t addr, size_t len, uint32_t data){
     uint32_t offset = addr & (Cache_Block_L1_Size - 1);
 
     int i,group = group_idx * Cache_L1_Way_Size;
-    for (i = group + 0;i < group + Cache_L1_Way_Size;i ++){
+    for (i = group;i < group + Cache_L1_Way_Size;i ++){
         if (cache1[i].valid == 1 && cache1[i].tag == tag){// WRITE HIT
             /*write through*/
             if (offset + len > Cache_Block_L1_Size)
             {
                 dram_write(addr,Cache_Block_L1_Size - offset,data);
                 memcpy(cache1[i].data + offset, &data, Cache_Block_L1_Size - offset);
+                write_cache2(addr,Cache_Block_L1_Size - offset,data);
                 write_cache1(addr + Cache_Block_L1_Size - offset, len -  (Cache_Block_L1_Size - offset), data >>  (Cache_Block_L1_Size - offset));
             }
             else
             {
                 dram_write(addr,len,data);
                 memcpy(cache1[i].data + offset, &data, len);
-                
+                write_cache2(addr,len,data);
             }
             //printf("%d\n", test_time);
             return;
         }
     }
-    dram_write(addr,len,data);
+    //dram_write(addr,len,data);
+    write_cache2(addr,len,data);
+}
+
+void write_cache2(hwaddr_t addr, size_t len, uint32_t data){
+    uint32_t group_idx = (addr >> Cache_L2_Block_Bit) & (Cache_L2_Group_Size - 1);
+    uint32_t tag = (addr >> (Cache_L2_Group_Bit + Cache_L2_Block_Bit));
+    uint32_t offset = addr & (Cache_Block_L2_Size - 1);
+
+    int i,group = group_idx * Cache_L2_Way_Size;
+    for (i = group + 0;i < group + Cache_L2_Way_Size;i ++){
+        if (cache2[i].valid == 1 && cache2[i].tag == tag)
+        {   // WRITE HIT
+            cache2[i].dirty = 1;
+            if (offset + len > Cache_Block_L2_Size)
+            {
+                memcpy(cache2[i].data + offset, &data, Cache_Block_L2_Size - offset);
+                write_cache2(addr + Cache_Block_L2_Size - offset,len - (Cache_Block_L2_Size - offset),data >> (Cache_Block_L2_Size - offset));
+            }
+            else
+                memcpy(cache2[i].data + offset, &data, len);
+            return;
+        }
+    }
+    /*write allocate*/
+    i = read_cache2(addr);
+    cache2[i].dirty = 1;
+    memcpy(cache2[i].data + offset,&data,len);
 }
