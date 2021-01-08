3fc4fa88127240a57e8a6dcf139e46e811e1d473
tracer-ics2016 <tracer@njuics.org>
2021-01-08 14:45:30 +0800

    >  compile NEMU 3019244255 klx Linux klx-virtual-machine 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:45:30 up  1:11,  1 user,  load average: 0.31, 0.37, 0.40 cd8c1286a73bdf983bea1fa7551d482da6eeb9

--- nemu/src/cpu/exec/control-tran/jmp-template.h
+++ nemu/src/cpu/exec/control-tran/jmp-template.h
@@ -25,51 +25,22 @@ Sreg_Descriptor new_sreg_desc;
 make_helper(ljmp){
     sreg_desc = &new_sreg_desc;
 
-    uint32_t op1 = instr_fetch(cpu.eip + 1,4);
-    uint16_t op2 = instr_fetch(cpu.eip + 1 + 4,2);
-    
-    cpu.eip = op1 - 7;
-    cpu.cs.selector = op2;
+    cpu.eip = instr_fetch(cpu.eip + 1, 4) - 7;
 
-    //current_sreg = R_CS;
-
-    // printf("%x\n",cpu.eip);
-    // printf("%x\n",instr_fetch(cpu.eip,1));
     uint16_t idx = cpu.cs.selector >> 3;//index of sreg
 
-	//Assert((idx << 3) <= cpu.gdtr.limit,"Segement Selector Is Out Of The Limit!");
+	lnaddr_t chart_addr = cpu.gdtr.base + (idx << 3);//chart addr
     
-	uint32_t chart_addr = cpu.gdtr.base + (idx << 3);//chart addr
-    
-	sreg_desc -> part1 = lnaddr_read(chart_addr, 4);
-	sreg_desc -> part2 = lnaddr_read(chart_addr + 4, 4);
+	sreg_desc->part1 = lnaddr_read(chart_addr, 4);
+	sreg_desc->part2 = lnaddr_read(chart_addr + 4, 4);
 
-    // printf("%x     %x\n",sreg_desc -> part1,sreg_desc -> part2);
-
-	//Assert(sreg_desc -> p == 1, "Segement Not Exist!");//p bit, whether sreg_desc exists
-
-    uint32_t bases = 0;
-	
-    //printf("%x %x %x\n",sreg_desc -> base1,sreg_desc -> base2,sreg_desc -> base3);
-
-	bases |= ((uint32_t)sreg_desc -> base1);
-	bases |= ((uint32_t)sreg_desc -> base2)<< 16;
-	bases |= ((uint32_t)sreg_desc -> base3)<< 24;
-	//printf("%x\n",instr_fetch(cpu.eip,1));
-    cpu.cs.base = bases;
-
-    //printf("%x\n",instr_fetch(cpu.eip,1));
-
-	uint32_t limits = 0;
-	limits |= ((uint32_t)sreg_desc -> limit1);
-	limits |= ((uint32_t)sreg_desc -> limit2) << 16;
-	limits |= ((uint32_t)0xfff) << 24;
-    cpu.cs.limit = limits;
+    cpu.cs.base = sreg_desc->base1 + (sreg_desc->base2 << 16) + (sreg_desc->base3 << 24);
+    cpu.cs.limit =  sreg_desc->limit1 + (sreg_desc->limit2 << 16) + (0xfff << 24);
 
 
-	if (sreg_desc -> g == 1) cpu.cs.limit <<= 12;//G = 0, unit = 1B;G = 1, unit = 4KB
-    print_asm("ljump %x %x",op2,op1);
-    return 1 + 6;    
+	if (sreg_desc->g == 1) cpu.cs.limit <<= 12;//G = 0, unit = 1B;G = 1, unit = 4KB
+    print_asm("ljump %x %x",instr_fetch(cpu.eip+1 + 4, 2),instr_fetch(cpu.eip+1, 4));
+    return 7;    
 }
 #endif
 
